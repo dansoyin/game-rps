57d227a0bfa9785bc1d48211d9dc941747e4cd6d
dansoyin <tukangs0m4y@gmail.com>
2026-03-10 15:20:19 +0700

    Update gem1.py

--- gem1.py
+++ gem1.py
@@ -9,60 +9,60 @@ rock = '''
 ---.__(___)
 rock
 '''
-papper = '''
+paper = '''
     _______
 ---'   ____)____
           ______)
           _______)
          _______)
 ---.__________)
-papper
+paper
 '''
-sciccors = '''
+scissors = '''
     _______
 ---'   ____)____
           ______)
        __________)
       (____)
 ---.__(___)
-sciccors
+scissors
 '''
 
-list = [rock,papper,sciccors]
-ngacak = random.choice(list)
+game_image = [rock,paper,scissors]
+ngacak = random.choice(game_image)
 
-user_choise = int(input("What do you choose? Type 0 for Rock, 1 for Papper or 2 for Sciccors.\n"))
-if user_choise == 0 :
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for paper or 2 for scissors.\n"))
+if user_choice <0 or user_choice > 2:
+    print("Invalid Number")
+if user_choice == 0 :
     print(rock)
     if ngacak == rock:
         print(ngacak)
         print("It's draw")
-    elif ngacak == papper:
+    elif ngacak == paper:
         print(ngacak)
         print("You lose")
     else:
         print("You win !")
-elif user_choise == 1:
-    print(papper)
+elif user_choice == 1:
+    print(paper)
     if ngacak == rock:
         print(ngacak)
         print("You win!")
-    elif ngacak == papper:
+    elif ngacak == paper:
         print(ngacak)
         print("It's draw")
     else:
         print(ngacak)
         print("You lose")
-elif user_choise == 2:
-    print(sciccors)
+elif user_choice == 2:
+    print(scissors)
     if ngacak == rock:
         print(ngacak)
         print("You lose")
-    elif ngacak == papper:
+    elif ngacak == paper:
         print(ngacak)
         print("You win!")
     else:
         print(ngacak)
         print("It's draw")
-
-# print(ngacak)
